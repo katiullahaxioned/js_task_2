a24d0e580de3b1cda541c7dbb1290a3fc5ca4cac
katiullahaxioned <atiullahk@axioned.com>
2022-08-13 10:46:35 +0530

    function added for common functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,50 @@
-var step = document.querySelectorAll('.step');
-var trackTop = document.querySelector('.track-top');
-var prev = document.querySelector('.prev');
-var next = document.querySelector('.next');
-var stepCircle = document.querySelectorAll('.step-circle');
+window.addEventListener('load', function () {
+	var trackTop = document.querySelector('.track-top');
+	var prev = document.querySelector('.prev');
+	var next = document.querySelector('.next');
+	var stepCircle = document.querySelectorAll('.step-circle');
+	var initialnum = 0;
+	var a = 0;
 
-var initialnum = 0;
+	function stepCircleShow(num) {
 
-function stepCircleShow(num) {
+		for (var y of stepCircle) {
+			y.classList.add('visibilityHidden');
+		}
+		stepCircle[num].classList.remove('visibilityHidden');
 
-	for(var y of stepCircle) {
-		y.style.visibility = 'hidden';
+		if (num >= 1) {
+			prev.classList.remove('bg-event');
+		} else {
+			prev.classList.add('bg-event');
+		}
+
+		if (num == stepCircle.length - 1) {
+			next.classList.add('bg-event');
+		} else {
+			next.classList.remove('bg-event');
+		}
+
 	}
-	stepCircle[num].style.visibility = 'visible';
 
-	if(num >= 1) {
-		prev.style.backgroundColor = 'var(--clr-blue)';
-		prev.style.pointerEvents = 'auto';
-	}else{
-		prev.style.backgroundColor = 'var(--clr-grey2)';
-		prev.style.pointerEvents = 'none';
-	}
-
-	if(num == stepCircle.length-1) {
-		next.style.backgroundColor = 'var(--clr-grey2)';
-		next.style.pointerEvents = 'none';
-	}else{
-		next.style.backgroundColor = 'var(--clr-blue)';
-		next.style.pointerEvents = 'auto';
-	}
-}
-
-stepCircleShow(initialnum);
-
-var a = 0;
-
-next.addEventListener('click', function() {
-	var x = 1;
-	initialnum += x;
-	a += 33.33;
-	trackTop.style.width = a + '%';
 	stepCircleShow(initialnum);
-	for(var i=0; i<=initialnum; i++) {
-		stepCircle[i].style.visibility = 'visible';
-	}
-})
 
-prev.addEventListener('click', function() {
-	var x = (-1);
-	initialnum += x;
-	a -= 33.33;
-	trackTop.style.width = a + '%';
-	stepCircleShow(initialnum);
-	for(var i=0; i<=initialnum; i++) {
-		stepCircle[i].style.visibility = 'visible';
+	function controlBtn(x, b) {
+		initialnum += x;
+		a += b;
+		trackTop.style.width = a + '%';
+		stepCircleShow(initialnum);
+		for (var i = 0; i <= initialnum; i++) {
+			stepCircle[i].classList.remove('visibilityHidden');
+		}
 	}
+
+	next.addEventListener('click', function () {
+		controlBtn(1, 33.33);
+	})
+
+	prev.addEventListener('click', function () {
+		controlBtn((-1), (-33.33));
+	})
+
 })
